332691664d20a42179135bfa768cc3d430e2a64a
NineSwordsMonster <wangjia_1919@163.com>
2020-09-21 18:05:47 +0800

    style:(lint) fix
    fix code style

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { path } = require('@vuepress/shared-utils')
 
-module.exports = (options, context) => ({
+module.exports = () => ({
   name: '@vuepress-reco/vuepress-plugin-screenfull',
   enhanceAppFiles: [
     path.resolve(__dirname, './bin/enhanceAppFile.js')
